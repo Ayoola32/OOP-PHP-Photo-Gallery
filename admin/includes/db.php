a688095e780ce656274e0f393ba7fc46b09decf8
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-07-14 13:43:22 +0100

    change the connection methods and syntax to be more oop- by using the new mysqli key

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -11,9 +11,11 @@ class Database {
     }
 
     public function open_db_connection() {
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if (mysqli_connect_errno()) {
-            die("Database Connection Failed " . mysqli_error($this->connection));
+        // $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        if ($this->connection->connect_errno) {
+            die("Database Connection Failed " . $this->connection->connect_error);
         }
        
     }
@@ -21,19 +23,19 @@ class Database {
 
 
     public function query($sql){
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
         $this->confirm_query($result);
         return $result;
     }
 
     private function confirm_query($result){
         if (!$result) {
-            die("Query Failed" . mysqli_error($this->connection));
+            die("Query Failed" . $this->connection->error);
         }
     }
 
     public function escape_string($string){
-        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+        $escaped_string = $this->connection->real_escape_string($string);
         return $escaped_string;
     }
 
