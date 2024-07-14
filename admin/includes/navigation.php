0506278ef9596a463612a33c617efab2e1eed3fc
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-07-14 10:05:13 +0100

    files added for admin sidebar pages

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -32,29 +32,29 @@
                 </li>
             </ul>
 
-            
+
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li>
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-user"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="users.php">View all Users</a>
                             </li>
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="#">Add Users</a>
                             </li>
                         </ul>
                     </li>
                     <li class="">
-                        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+                        <a href="upload.php"><i class="fa fa-fw fa-file"></i> Upload</a>
                     </li>
                     <li>
-                        <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
+                        <a href="comments.php"><i class="fa fa-fw fa-dashboard"></i> Comments</a>
                     </li>
                 </ul>
             </div>
