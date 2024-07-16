71a436204dcbfd881a9500d93d6fc4383f215b7d
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-07-16 21:08:41 +0100

    login page added to homepage

---
+++ login.php
@@ -0,0 +1,62 @@
+<?php  include "includes/header.php"; ?>
+<?php require_once "admin/classes/init.php"; ?>
+
+<!-- Navigation -->
+
+<?php  include "includes/navigation.php"; ?>
+
+
+<!-- Page Content -->
+<div class="container">
+
+	<div class="form-gap"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="text-center">
+							<h2 class="text-center">Login</h2>
+
+							<div class="panel-body">
+								<form action="./includes/login.php" id="login-form" role="form" autocomplete="off" class="form" method="post">
+
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
+
+											<input name="username" type="text" class="form-control" placeholder="Enter Username">
+										</div>
+									</div>
+
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-lock color-blue"></i></span>
+											<input name="password" type="password" class="form-control" placeholder="Enter Password">
+										</div>
+									</div>
+
+									<div class="form-group">
+
+										<input name="submit" class="btn btn-lg btn-primary btn-block" value="Login" type="submit">
+									</div>
+
+									<div class="form-group">
+										<a href="forgot_password.php?forgot=<?php echo uniqid(true) ?>" class="text-center">Forgot Password</a>
+									</div>
+								</form>
+
+							</div><!-- Body-->
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<hr>
+
+	<?php include "includes/footer.php";?>
+
+</div> <!-- /.container -->
