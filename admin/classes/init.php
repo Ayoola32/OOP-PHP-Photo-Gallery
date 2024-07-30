3cfbe22c57b425425bf87d96129d8c059b045763
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-07-30 18:35:12 +0100

    added photo class file and its properties

--- admin/classes/init.php
+++ admin/classes/init.php
@@ -3,6 +3,7 @@ require_once "function.php";
 require_once "new_config.php";
 require_once "db.php";
 require_once "db_object.php";
+require_once "photo.php";
 require_once "user.php";
 require_once "session.php";
 
---
+++ admin/classes/photo.php
@@ -0,0 +1,22 @@
+<?php
+
+class Photo extends Db_object{
+
+    protected static $db_table = "photos";
+    protected static $db_table_field = array('photo_id', 'title', 'description', 'filename', 'type', 'size');
+    public $photo_id;
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
+
+
+
+
+}
+
+
+
+
+?>
