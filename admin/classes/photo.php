20de16bdfaa3e5639e76c0c0e99124dc5a3886e0
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-07-31 17:11:48 +0100

    more photo properties added

--- admin/classes/photo.php
+++ admin/classes/photo.php
@@ -10,6 +10,19 @@ class Photo extends Db_object {
     public $type;
     public $size;
 
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $upload_error = array(
+        UPLOAD_ERR_OK => "There is no error",
+        UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max filesize directive",
+        UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive",
+        UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded",
+        UPLOAD_ERR_NO_FILE => "No file was uploaded",
+        UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder",
+        UPLOAD_ERR_CANT_WRITE => "Failed to write file to a disk",
+        UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload"
+    );
+
 
 
 
