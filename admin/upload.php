0c60c9b8e6fcdc401c554c83907fcee015144588
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-03 10:01:39 +0100

    test photo class by uploading an image

--- admin/upload.php
+++ admin/upload.php
@@ -2,6 +2,30 @@
 <?php include "includes/navigation.php"; ?>
 
       
+<?php
+$message = "";
+if (isset($_POST["submit"])) {
+    $photo = new Photo();
+    $photo->title = $_POST['title'];
+    $photo->description = $_POST['description'];
+    $photo->set_file($_FILES['file_upload']);
+
+
+
+    if ($photo->save()) {
+        $message = "Photo Upload Successfully";
+    }else{
+        $message = join("<br>", $photo->errors);
+    }
+
+}
+
+
+
+
+?>
+
+
 
         <div id="page-wrapper">
 
@@ -23,24 +47,32 @@
                             </li>
                         </ol>
 
-                        <form action="" method="post" enctype="multipart/form-data">
-                            <h2>Add New Photo</h2>
+                        <div class="col-md-4">
+                            <?php echo $message?>
+                            <form action="upload.php" method="post" enctype="multipart/form-data">
+                                <h2>Add New Photo</h2>
+                                
+                                
+                                <div class="form-group">
+                                    <label for="title">Photo Title</label>
+                                    <input type="text" class="form-control" name="title">
+                                </div>
 
-
-                            <div class="form-group">
-                                <label for="title">Photo Title</label>
-                                <input type="text" class="form-control" name="title">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="file_upload">Photo Upload</label>
-                                <input type="file" name="file_upload">
-                            </div>
-
-                            <div class="form-group">
-                                <input class= "btn btn-primary" type="submit" name="submit" value="Add Photo">
-                            </div>
-                        </form>
+                                <div class="form-group">
+                                    <label for="description">Photo Description</label>
+                                    <input type="text" class="form-control" name="description">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="file_upload">Photo Upload</label>
+                                    <input type="file" name="file_upload">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <input class= "btn btn-primary" type="submit" name="submit" value="Add Photo">
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
