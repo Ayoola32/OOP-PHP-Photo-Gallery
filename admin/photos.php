a92d0721f9335dd7c3ed8c65d08fcde30efb7bee
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-03 10:28:45 +0100

    created a table to view the pictures uploaded in the database

--- admin/photos.php
+++ admin/photos.php
@@ -1,6 +1,7 @@
 <?php include "includes/header.php"; ?>
 <?php include "includes/navigation.php"; ?>
 
+<?php $photos = Photo::find_all();?>
       
 
         <div id="page-wrapper">
@@ -22,6 +23,42 @@
                                 <i class="fa fa-file"></i> Blank Page
                             </li>
                         </ol>
+
+                        
+                        <div class="col-md-12">
+                            <table class="table table-bordered hovered table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Photo</th>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                        <th>Filename</th>
+                                        <th>Type</th>
+                                        <th>Size</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($photos as $photo) : ?>
+                                        <tr>
+                                            <td><?php echo $photo->photo_id?></td>
+                                            <td><img src="http://placehold.it/62x62" alt=""></td>
+                                            <td><?php echo $photo->title?></td>
+                                            <td><?php echo $photo->description?></td>
+                                            <td><?php echo $photo->filename?></td>
+                                            <td><?php echo $photo->type?></td>
+                                            <td><?php echo $photo->size?></td>
+                                        </tr>
+                                    <?php endforeach;?>
+                                </tbody>
+                            </table> 
+                        </div>
+
+
+
+
+
+
                     </div>
                 </div>
                 <!-- /.row -->
