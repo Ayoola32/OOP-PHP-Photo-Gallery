8440879718f0dcab8883df81b490078849256b83
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-03 23:35:21 +0100

    modified the delete.php script to work well with the case method

--- admin/includes/photo_delete.php
+++ admin/includes/photo_delete.php
@@ -1,26 +1,21 @@
 <?php include "../classes/init.php"; ?>
-<?php
-if (!$session->is_signed_in()) {
-    redirect("../../index.php");
-}
-?>
 
 <?php
 if (empty($_GET['photo_id'])) {
-    header("Location: ../../photos.php");
+    header("Location: ../photos.php"); 
     exit(); 
 }
 
 
 $photo = Photo::find_by_id($_GET['photo_id']);
 if ($photo) {
-    $photo->delete_photo();
-    header("Location: ../../photos.php");
+    $photo->delete_photo();  
+    header("Location: ../photos.php"); 
     exit();
 } else {
-    redirect("../../photos.php");
+    header("Location: ../photos.php"); 
+    exit();
 }
-?>
 
 
 
