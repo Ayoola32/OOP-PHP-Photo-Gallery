d53d3b55af57559e90265c483ef5b6e831b2f1ca
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-04 08:33:05 +0100

    added a style to the update sidebar

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -33,20 +33,22 @@ if (!$session->is_signed_in()) {
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    
     <!-- Custom CSS -->
     <link href="css/sb-admin.css" rel="stylesheet">
-
+    
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
+    
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+        <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+        <![endif]-->
+        
+        <!--CSS -->
+        <link href="css/styles.css" rel="stylesheet">
 </head>
 
 <body>
