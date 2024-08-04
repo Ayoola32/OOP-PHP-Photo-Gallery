98130bc5f09dc7adef0e228161222a1155fe4ab8
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-04 16:30:44 +0100

    user view all page created

---
+++ admin/includes/user_view_all.php
@@ -0,0 +1,33 @@
+<?php $users = User::find_all();?>
+
+<table class="table  hovered table-hover table-striped">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Image</th>
+            <th>Username</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($users as $user) : ?>
+            <tr>
+                <td><?php echo $user->user_id?></td>
+                <td>
+                    <img src="<?php echo $user->image_path_placeholder(); ?>" alt="" style="width: 150px; height: auto;">
+                    <div class="picture_link">
+                        <a class='btn-warning' href=''>View</a>
+                        <a class='btn-info mr-2' href='users.php?source=user_update&photo_id=<?php echo $photo->photo_id;?>'>Edit</a>
+                        <a class='btn-danger' href='users.php/?source=user_delete&photo_id=<?php echo $photo->photo_id;?>'>Delete</a>
+                    </div>
+                </td>
+                <td><?php echo $user->username?></td>
+                <td><?php echo $user->first_name?></td>
+                <td><?php echo $user->last_name?></td>
+                <td><?php echo $user->user_email?></td>
+            </tr>
+        <?php endforeach;?>
+    </tbody>
+</table> 
