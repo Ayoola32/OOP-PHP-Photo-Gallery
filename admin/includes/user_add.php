fdc4ef1b852d1111aca386e475a1f9a17f7d2d30
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-04 18:25:56 +0100

    user_add form query completed, uploading of image and display of error messages

--- admin/includes/user_add.php
+++ admin/includes/user_add.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $message = "";
 if (isset($_POST["submit"])) {
     $user = new User();
@@ -9,12 +8,25 @@ if (isset($_POST["submit"])) {
     $user->username = $_POST['username'];
     $user->password = $_POST['password'];
     $user->user_email = $_POST['email'];
+    $user->set_file($_FILES['user_image']);
 
-    if ($user->save()) {
-        $message = "New user added Successfully";
-    }else{
-        $message = join("<br>", $user->errors);
+    if ($user->has_empty_fields()) {
+        $message = "<p class='alert alert-danger'>All fields are required.</p>";
+    } else {
+        $existing_user = User::username_or_email_exists($user->username, $user->user_email);
+        
+        if ($existing_user) {
+            $message = "<p class='alert alert-danger'>Username or Email has already been taken.</p>";
+        } else {
+            // Save the new user
+            if ($user->save()) {
+                $message = "<p class='alert alert-success'>User created successfully.</p>";
+            } else {
+                $message = "<p class='alert alert-danger'>Failed to create user.  " . implode("<br>", $user->errors) . "</p>";
+            }
+        }
     }
+
 
 }
 
@@ -25,12 +37,11 @@ if (isset($_POST["submit"])) {
 
 
 <div class="col-md-4">
-    <?php echo $message?>
+    <?php echo $message; ?>
     <form action="" method="post" enctype="multipart/form-data">
         <h2>Add New User</h2>
         
-        
-        <div class="form-group">
+    <div class="form-group">
         <label for="firstname">Firstname</label>
         <input type="text" class="form-control" name="firstname">
     </div>
@@ -55,13 +66,13 @@ if (isset($_POST["submit"])) {
         <input type="password" class="form-control" name="password">
     </div>
 
-    <!-- <div class="form-group">
+    <div class="form-group">
         <label for="user_image">User Image</label>
         <input type="file" name="user_image">
-    </div> -->
+    </div>
 
     <div class="form-group">
-        <input class= "btn btn-primary" type="submit" class="form-control" name="submit" value="Add User">
+        <input class="btn btn-primary" type="submit" name="submit" value="Add User">
     </div>
     </form>
 </div>
