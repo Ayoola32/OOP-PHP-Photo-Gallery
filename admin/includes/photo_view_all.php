e38b2c1731cd0e388e9efbed726b03a587c8d9a7
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-04 08:26:42 +0100

    adjusted the layout of the photo_view_all table

--- admin/includes/photo_view_all.php
+++ admin/includes/photo_view_all.php
@@ -1,33 +1,34 @@
 <?php $photos = Photo::find_all();?>
 
-<table class="table table-bordered hovered table-hover table-striped">
+<table class="table  hovered table-hover table-striped">
     <thead>
         <tr>
-            <th>Id</th>
             <th>Photo</th>
+            <th>Id</th>
             <th>Title</th>
-            <th>Description</th>
             <th>Filename</th>
-            <th>Type</th>
             <th>Size</th>
-            <th>View</th>
-            <th>Edit</th>
-            <th>Delete</th>
+            <!-- <th>Description</th> -->
+            <!-- <th>Type</th> -->
         </tr>
     </thead>
     <tbody>
         <?php foreach($photos as $photo) : ?>
             <tr>
+                <td>
+                    <img src="<?php echo $photo->picture_path(); ?>" alt="" style="width: 200px; height: auto;">
+                    <div class="picture_link">
+                        <a class='btn-warning' href=''>View</a>
+                        <a class='btn-info mr-2' href='photos.php?source=photo_update&photo_id=<?php echo $photo->photo_id;?>'>Edit</a>
+                        <a class='btn-danger' href='photos.php/?source=photo_delete&photo_id=<?php echo $photo->photo_id;?>'>Delete</a>
+                    </div>
+                </td>
                 <td><?php echo $photo->photo_id?></td>
-                <td><img src="<?php echo $photo->picture_path(); ?>" alt="" style="width: 100px; height: auto;"></td>
                 <td><?php echo $photo->title?></td>
-                <td><?php echo $photo->description?></td>
                 <td><?php echo $photo->filename?></td>
-                <td><?php echo $photo->type?></td>
                 <td><?php echo $photo->size?></td>
-                <td><a class='btn btn-warning' href=''>View</a></td>
-                <td><a class='btn btn-info mr-2' href='photos.php?source=photo_update&photo_id=<?php echo $photo->photo_id;?>'>Edit</a></td>
-                <td><a class='btn btn-danger' href='photos.php/?source=photo_delete&photo_id=<?php echo $photo->photo_id;?>'>Delete</a></td>
+                <td><?php //echo $photo->description?></td>
+                <td><?php //echo $photo->type?></td>
             </tr>
         <?php endforeach;?>
     </tbody>
