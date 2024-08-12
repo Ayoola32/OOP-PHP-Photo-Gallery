f38fef23345888a17b7177236da0fb572ec8e33b
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-12 20:16:57 +0100

    added date column to the database and also to comment class

--- admin/classes/comment.php
+++ admin/classes/comment.php
@@ -2,13 +2,14 @@
 
 class Comment extends Db_object {
     protected static $db_table = "comments";
-    protected static $db_table_field = array('photo_id', 'author', 'content');
+    protected static $db_table_field = array('photo_id', 'author', 'content', 'date');
     protected static $db_id_field = "comment_id"; // Specify the id field for the Comment class
 
     public $comment_id;
     public $photo_id;
     public $author;
     public $content;
+    public $date;
 
     // Self-instantiation comment method
     public static function create_comment($photo_id, $author, $content){
@@ -17,6 +18,7 @@ class Comment extends Db_object {
             $comment->photo_id = (int)$photo_id;
             $comment->author   = $author;
             $comment->content  = $content;
+            $comment->date     = date('Y-m-d H:i:s');
 
             return $comment;
         } else {
