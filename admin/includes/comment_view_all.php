6eea91d2bebc1b3839e784fb4a6d67e6e77f6cbb
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-14 18:09:59 +0100

    created comment view all page

---
+++ admin/includes/comment_view_all.php
@@ -0,0 +1,29 @@
+<?php $comments = Comment::find_all();?>
+
+<table class="table  hovered table-hover table-striped">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Author</th>
+            <th>Content</th>
+            <th>Date</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($comments as $comment) : ?>
+            <tr>
+                <td><?php echo $comment->comment_id?></td>
+                <td><?php echo $comment->author?></td>
+                <td><?php echo $comment->content?></td>
+                <td><?php echo date('F d, Y \a\t g:i A', strtotime($comment->date));?></td>
+                <td>
+                    <div class="picture_link">
+                        <a class='btn-warning' href=''>View</a>
+                        <a class='btn-danger' href='comments.php?source=comment_delete&comment_id=<?php echo $comment->user_id;?>'>Delete</a>
+                    </div>
+                </td>
+            </tr>
+        <?php endforeach;?>
+    </tbody>
+</table> 
