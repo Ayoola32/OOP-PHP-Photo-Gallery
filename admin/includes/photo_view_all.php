15c970aed4094f3a24e6488096166997f8b7b91d
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-14 21:06:28 +0100

    display comment count adn directing to the link to view the comments for the specific photo posted

--- admin/includes/photo_view_all.php
+++ admin/includes/photo_view_all.php
@@ -1,4 +1,4 @@
-<?php $photos = Photo::find_all();?>
+<?php $photos = Photo::find_all(); ?>
 
 <table class="table  hovered table-hover table-striped">
     <thead>
@@ -13,6 +13,7 @@
     </thead>
     <tbody>
         <?php foreach($photos as $photo) : ?>
+            <?php $comments = Comment::find_the_comments($photo->photo_id);?>
             <tr>
                 <td>
                     <img src="<?php echo $photo->picture_path(); ?>" alt="" style="width: 200px; height: auto;">
@@ -26,7 +27,7 @@
                 <td><?php echo $photo->title?></td>
                 <td><?php echo $photo->filename?></td>
                 <td><?php echo $photo->size?></td>
-                <td><a href="photos.php?source=photo_comment&photo_id=<?php echo $photo->photo_id ?>">View all comments</a></td>
+                <td><a class='btn btn-warning' href="photos.php?source=photo_comment&photo_id=<?php echo $photo->photo_id ?>"><?php echo count($comments)?> comments</a></td>
             </tr>
         <?php endforeach;?>
     </tbody>
