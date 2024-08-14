e50b60a1f45978c7f2d0ed40bd8eb897063639c2
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-14 19:53:16 +0100

    comment view all table to include photo_id for each comments

--- admin/includes/comment_view_all.php
+++ admin/includes/comment_view_all.php
@@ -4,6 +4,7 @@
     <thead>
         <tr>
             <th>Id</th>
+            <th>Photo Id</th>
             <th>Author</th>
             <th>Content</th>
             <th>Date</th>
@@ -14,13 +15,14 @@
         <?php foreach($comments as $comment) : ?>
             <tr>
                 <td><?php echo $comment->comment_id?></td>
+                <td><?php echo $comment->photo_id?></td>
                 <td><?php echo $comment->author?></td>
                 <td><?php echo $comment->content?></td>
                 <td><?php echo date('F d, Y \a\t g:i A', strtotime($comment->date));?></td>
                 <td>
                     <div class="picture_link">
-                        <a class='btn-warning' href=''>View</a>
-                        <a class='btn-danger' href='comments.php?source=comment_delete&comment_id=<?php echo $comment->comment_id;?>'>Delete</a>
+                    <!-- <a class='btn-warning' href='../post_view.php?photo_id=<?php //echo $comment->photo_id?>'>View</a> -->
+                    <a class='btn-danger' href='comments.php?source=comment_delete&comment_id=<?php echo $comment->comment_id;?>'>Delete</a>
                     </div>
                 </td>
             </tr>
