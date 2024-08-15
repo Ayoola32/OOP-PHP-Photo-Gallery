e7e0251d45a4853401a1b3e98d2a319a9e917e7d
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-15 23:44:46 +0100

    modified the delete query to handle botht the query sent from comment view all and photo_comment and stay on the same page respectively after deletion

--- admin/includes/comment_delete.php
+++ admin/includes/comment_delete.php
@@ -1,24 +1,31 @@
 <?php include "../admin/classes/init.php"; ?>
 
 <?php
-if (empty($_GET['comment_id'])) {
-    header("Location: ../admin/comments.php"); 
-    exit(); 
-}
-
-
-$comment = Comment::find_by_id($_GET['comment_id']);
-if ($comment) {
-    $comment->delete();  
-    header("Location: ../admin/photos.php?source=photo_comment&photo_id=$comment->photo_id"); 
-    exit();
+if (isset($_GET['comment_id'])) {
+    $comment = Comment::find_by_id($_GET['comment_id']);
+    if ($comment) {
+        $comment->delete();
+        
+        if (isset($_GET['photo_id'])) {
+            header("Location: ../admin/photos.php?source=photo_comment&photo_id=" . $_GET['photo_id']); 
+        } else {
+            header("Location: ../admin/comments.php"); 
+        }
+        exit();
+    } else {
+        if (isset($_GET['photo_id'])) {
+            header("Location: ../admin/photos.php?source=photo_comment&photo_id=" . $_GET['photo_id']);
+        } else {
+            header("Location: ../admin/comments.php");
+        }
+        exit();
+    }
 } else {
-    header("Location: ../admin/comments.php"); 
+    if (isset($_GET['photo_id'])) {
+        header("Location: ../admin/photos.php?source=photo_comment&photo_id=" . $_GET['photo_id']);
+    } else {
+        header("Location: ../admin/comments.php");
+    }
     exit();
 }
-
-
-
-
-
 ?>
