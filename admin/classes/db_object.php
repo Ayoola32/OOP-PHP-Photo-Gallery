2b3e48c8612139870d3b8993e632c877d72e5ebe
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-17 09:05:51 +0100

    count all datat query added to db_object class

--- admin/classes/db_object.php
+++ admin/classes/db_object.php
@@ -146,6 +146,17 @@ class Db_object {
         return (mysqli_affected_rows($database->connection) == 1) ? true : false;
     }
 
+
+    // Count all data
+    public static function count_all(){
+        global $database;
+        $sql = "SELECT COUNT(*) FROM ". static::$db_table;
+        $result_set = $database->query($sql);
+        $row = mysqli_fetch_array($result_set);
+
+        return array_shift($row);
+    }
+
     
 
 
