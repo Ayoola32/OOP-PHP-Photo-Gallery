49b01e68fb130e6140873b12dcb5eb8b0e1fa90d
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-17 15:23:09 +0100

    styles added for index photo display

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,9 @@
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
 
+    <!-- CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
