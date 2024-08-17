b650a25ce23ca913f15eb7e5be6293eed1f00806
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-17 15:21:57 +0100

    display picture in frontend

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php include "includes/header.php";?>
 <?php include "includes/navigation.php";?>
 
+
+<?php $photos = Photo::find_all();?>
+
     <!-- Page Content -->
     <div class="container">
 
@@ -8,10 +11,26 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-                <?php
-                   
+                <h2>All Photos Blog</h2>
+
+                <div class="thumbnails row">
+                <?php foreach($photos as $photo) : ?>       
                     
-                ?>
+                    <div class="col-xs-10 col-md-5">
+                        <hr>
+                        <p><span class="glyphicon glyphicon-time"></span> <?php echo date('F d, Y \a\t g:i A', strtotime($photo->date));?></p>
+                        <a href="post_view.php?photo_id=<?php echo $photo->photo_id;?>" class="thumbnail">
+                            <img class="img_responsive home_page_picture"src="admin/<?php echo $photo->picture_path();?>" alt="" srcset="">
+                        </a>
+                        <h6 class="lead">
+                            by <a href="post_view.php?photo_id=<?php echo $photo->photo_id;?>"><?php echo $photo->author?></a>
+                        </h6>
+
+
+                    </div>        
+                <?php endforeach;?>
+                </div>
+
 
                 <!-- Pager -->
                 <ul class="pager">
