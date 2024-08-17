c5e4a3adcbbe6f2ce206fb2c2325962ee085e00e
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-17 09:05:08 +0100

    count query for how many times the page is visited added

--- admin/classes/session.php
+++ admin/classes/session.php
@@ -5,13 +5,24 @@ class Session{
     private $signed_in = false;
     public  $user_id;
     public  $message;
+    public  $count;
 
 
 
     function __construct(){
         session_start();
+        $this->visitor_count();
         $this->check_the_login();
         $this->check_message();
+    }
+
+
+    public function visitor_count(){
+        if (isset($_SESSION['count'])) {
+            return $this->count = $_SESSION['count']++;
+        }else {
+            return $_SESSION['count'] = 1;
+        }
     }
 
 
