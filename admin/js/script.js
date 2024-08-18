596b173dad2029823cf334ee5e552f2ed96f2df0
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-18 20:28:45 +0100

    ajax script tot handle the selecion of photos from the modal added

--- admin/js/script.js
+++ admin/js/script.js
@@ -4,15 +4,38 @@ $('#summernote').summernote({
   height: 200
 });
 
-
-
 $(document).ready(function() {
+    var user_id;
+    var user_href;
+    var user_href_splitted;
+
+    var image_name; 
+    var image_src;
+    var image_href_splitted;
+
     $(".modal_thumbnails").click(function(){
-        $("#set_user_image").prop('disabled', false);
+      $("#set_user_image").prop('disabled', false);
+      user_href          = $("#user_id").prop("href");
+      user_href_splitted = user_href.split("=");
+      user_id            = user_href_splitted[user_href_splitted.length - 1];
+
+      image_src           = $(this).prop("src");
+      image_href_splitted = image_src.split("/");
+      image_name          = image_href_splitted[image_href_splitted.length - 1];
+    });
+    
+    $("#set_user_image").click(function(){
+        $.ajax({
+          url: "includes/ajax_code.php",
+          data: {image_name: image_name, user_id: user_id},
+          type: "POST",
+          success: function(data){
+            if (!data.error) {
+              location.reload(true);
+            }
+          }
+        });
     });
 
-
-
-
-  tinymce.init({selector: 'textarea'});
+    tinymce.init({selector: 'textarea'});
 });
