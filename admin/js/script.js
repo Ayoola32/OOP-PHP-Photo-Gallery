e0d19fa8a90c167f96a75263936a9204ca2d1064
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-18 14:17:03 +0100

    script to handle the photo library  modal response

--- admin/js/script.js
+++ admin/js/script.js
@@ -3,3 +3,16 @@ $('#summernote').summernote({
   tabsize: 2,
   height: 200
 });
+
+
+
+$(document).ready(function() {
+    $(".modal_thumbnails").click(function(){
+        $("#set_user_image").prop('disabled', false);
+    });
+
+
+
+
+  tinymce.init({selector: 'textarea'});
+});
