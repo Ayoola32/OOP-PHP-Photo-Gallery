4e47940d7a43eec418bf00839c3e49884fa16e4a
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-19 19:30:53 +0100

    script code for the modal sidebar details for selected photo

--- admin/js/script.js
+++ admin/js/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
     var image_name; 
     var image_src;
     var image_href_splitted;
+    var photo_id;
 
     $(".modal_thumbnails").click(function(){
       $("#set_user_image").prop('disabled', false);
@@ -22,7 +23,23 @@ $(document).ready(function() {
       image_src           = $(this).prop("src");
       image_href_splitted = image_src.split("/");
       image_name          = image_href_splitted[image_href_splitted.length - 1];
+
+
+      
+      $photo_id = $(this).attr("src");
+      $.ajax({
+        url: "includes/ajax_code.php",
+        data: {photo_id: user_id},
+        type: "POST",
+        success: function(data){
+          if (!data.error) {
+            $("#modal_sidebar").html(data);
+          }
+        }
+      })
     });
+
+
     
     $("#set_user_image").click(function(){
         $.ajax({
